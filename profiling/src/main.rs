5fd7f3a20186b1509c0d83ff08e04f85cf392656
akpi816218 <gh@akpi.is-a.dev>
2024-07-09 14:22:20 -0400

    modifications

--- profiling/src/main.rs
+++ profiling/src/main.rs
@@ -42,9 +42,7 @@ fn main() {
 7, C, End
 8, main, End";
 
-	let input_arr: Vec<&str> = INPUT_S.split("\n").collect();
-
-	let entries: Vec<Entry> = map_to_entries(input_arr);
+	let entries = map_to_entries(INPUT_S.split("\n").collect());
 
 	let mut stack: Vec<Entry> = Vec::new();
 
@@ -68,24 +66,24 @@ fn map_to_entries(arr: Vec<&str>) -> Vec<Entry> {
 	let mut stack: Vec<Entry> = Vec::new();
 	for row in arr {
 		let cols: Vec<&str> = row.split(", ").collect();
-		let ts: f64 = match cols[0].parse::<f64>() {
-			Err(_) => panic!("Bad input"),
-			Ok(res) => res,
-		};
-		let name: &str = cols[1];
-		let typ: EventType = match cols[2] {
-			"Start" => EventType::Start,
-			"End" => EventType::End,
-			_ => panic!("Bad data"),
-		};
-
-		stack.push(Entry { ts, name, typ })
+		stack.push(Entry {
+			ts: match cols[0].parse::<f64>() {
+				Err(_) => panic!("Bad data"),
+				Ok(res) => res,
+			},
+			name: cols[1],
+			typ: match cols[2] {
+				"Start" => EventType::Start,
+				"End" => EventType::End,
+				_ => panic!("Bad data"),
+			},
+		})
 	}
 	stack
 }
 
 fn pad_line(depth: usize) -> String {
-	"\t".repeat(depth)
+	"> ".repeat(depth)
 }
 
 fn print_line(entry: &Entry, depth: usize) {
